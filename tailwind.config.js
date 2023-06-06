07c483efd27a3ccde7c95a053a4ccbb86115da31
Ersan Karimi <ersankarimii311@gmail.com>
2023-06-06 22:04:38 +0800

    feat: Update tailwind.config.js with theme and content props changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,42 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
+  content: ["./src/pages/**/*.{js,jsx}", "./src/components/**/*.{js,jsx}"],
   theme: {
     extend: {
+      fontFamily: {
+        "font-rubik": ["Rubik", "sans-serif"],
+      },
+      boxShadow: {
+        shadow: "0px 11px 30px rgba(0, 0, 0, 0.1)",
+      },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        linear:
+          "linear-gradient(180deg, #FFF2EA 0%, rgba(251, 228, 212, 0.742669) 25.73%, rgba(249, 249, 249, 0) 100",
+      },
+      colors: {
+        brand: {
+          primary: {
+            default: "#8BAC3E",
+            lighter: "#F0FEEB",
+            lightest: "#F9FFF6",
+          },
+          cyan: {
+            default: "#40A2B1",
+            lighter: "#E6F3F5",
+          },
+          indigo: {
+            default: "#405EB6",
+            lighter: "#EAEEFA",
+          },
+          magenta: {
+            default: "#B23F74",
+            lighter: "#F9EEF3",
+          },
+          lime: {
+            default: "#A4B441",
+            lighter: "#F3F7D9",
+          },
+        },
       },
     },
   },
