d2587b263a6e4e2185ff1a0dc971672007cbe9e9
Ersan Karimi <ersankarimii311@gmail.com>
2023-06-07 00:33:56 +0800

    feat: Add RootLayout wrapper to _app.jsx

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,5 +1,13 @@
+import { RootLayout } from "@/components";
+
 import "../styles/globals.css";
 
-const App = ({ Component, pageProps }) => <Component {...pageProps} />;
+const App = ({ Component, pageProps }) => {
+  return (
+    <RootLayout>
+      <Component {...pageProps} />
+    </RootLayout>
+  );
+};
 
 export default App;
