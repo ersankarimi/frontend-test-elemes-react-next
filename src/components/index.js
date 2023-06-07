43a223e6e69fa00da300e4e436d03a89407fe7d0
Ersan Karimi <ersankarimii311@gmail.com>
2023-06-07 15:42:24 +0800

    chore: re-exports the new created components

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,7 @@
 export * from "./root-layout";
 export * from "./navbar";
 export * from "./footer";
+export * from "./button";
+export * from "./stars";
+export * from "./category-receipt-card";
+export * from "./trending-receipt-card";
